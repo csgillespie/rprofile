092d313903fd499eca2772ad02afc29f1daebeeb
Colin Gillespie <csgillespie@gmail.com>
2022-12-03 18:28:22 +0000

    Add stripspace (#13)
    * feat: Add stripspace function
    
    * Update
    
    * Update

---
+++ R/stripspace.R
@@ -0,0 +1,12 @@
+# Uses git stripspace to clean files.
+# Not particularly elegant code. But a quick first pass to see if I like it
+stripspace = function() {
+  fnames = list.files(pattern = "\\.R$|\\.qmd$|\\.Rmd$|\\.md$", full.names = TRUE, recursive = TRUE)
+  tmp = tempfile()
+  for (fname in fnames) {
+    cli::cli_alert("Updating {fname}")
+    system2("git", c("stripspace", "<", fname, ">", tmp))
+    system2("cat", c(tmp, ">", fname))
+  }
+  return(invisible(NULL))
+}
